7b16a9ed2e6746859e52e02d00bf380f8ee24faf
Nicolas D'Amelio <ndamelio@capitaria.com>
2018-08-13 16:40:49 -0300

    first commit

---
+++ src/main/java/com/ndamelio/learning/springbootmicroservicescomments/SpringbootMicroservicesCommentsApplication.java
@@ -0,0 +1,12 @@
+package com.ndamelio.learning.springbootmicroservicescomments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroservicesCommentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMicroservicesCommentsApplication.class, args);
+    }
+}
